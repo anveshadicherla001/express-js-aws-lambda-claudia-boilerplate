9eab0c2b724e4e9449a759185194f0b3a8eea315
Anvesh <anveshadicherla001@gmail.com>
2024-07-10 01:16:14 +0530

    feat: Swagger API doc spec config added

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 require('dotenv').config({ path: '.env.development' });
 const app = express();
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware');
@@ -10,6 +12,7 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: false
 }));
 app.use(cors());
+app.disable('x-powered-by');
 const combiningReqParams = (req, res, next) => {
     const params = {...req.body,...req.query};
     req['allParams'] = params;
@@ -19,6 +22,50 @@ const combiningReqParams = (req, res, next) => {
 app.use(combiningReqParams);
 
 const db = require('./config/db');
+
+const options = {
+    definition: {
+        openapi: "3.1.0",
+        info: {
+            title: "Express API with Swagger",
+            version: "0.0.1",
+            description: "This is a simple CRUD API application made with Express and documented with Swagger",
+        },
+        servers: [
+            {
+                url: `${process.env.APP_URL}:${process.env.APP_PORT || 3000}/${process.env.APP_VER_PREFIX}`,
+                description: `${process.env.APP_ENV_NAME} Server`
+            },
+        ],
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                type: "apiKey",
+                name: "x-auth-token",
+                scheme: "bearer",
+                in: "header",
+                },
+            },
+        },
+        security: [
+            {
+                bearerAuth: {
+                    type: "http",
+                    scheme: "bearer",
+                },
+            },
+        ],
+    },
+    apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
+
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs, { explorer: true })
+);
 
 //Routes
 const routes = require("./routes");
--- routes/hello.js
+++ routes/hello.js
@@ -7,11 +7,131 @@ const  {
     welcome
 } = require('../controllers/hello.controller');
 
+
+/**
+ * @openapi
+ * '/hello':
+ *  get:
+ *     tags:
+ *     - Hello Controller
+ *     summary: Hello Get Route
+ *     responses:
+ *      200:
+ *        description: Fetched Successfully
+ *      400:
+ *        description: Bad Request
+ *      404:
+ *        description: Not Found
+ *      500:
+ *        description: Server Error
+ */
 /* Route without any middleware */
 router.get('/', welcome);
+/**
+ * @openapi
+ * '/hello/{name}':
+ *  get:
+ *     tags:
+ *     - Hello Controller
+ *     summary: Hello Get Route
+ *     parameters:
+ *      - name: name
+ *        in: path
+ *        description: The name (any string)
+ *        required: true
+ *     responses:
+ *      200:
+ *        description: Fetched Successfully
+ *      400:
+ *        description: Bad Request
+ *      404:
+ *        description: Not Found
+ *      500:
+ *        description: Server Error
+ */
 /* Route with validateSchema middleware */
 router.get('/:name', validateSchema('hello', 'welcome'), welcome);
+
 /* Route with isAuthenticated middleware */
 router.get('/protected/:name', validateSchema('hello', 'welcome'), isAuthenticated, welcome);
+
+/**
+ * @openapi
+ * '/hello/create':
+ *  post:
+ *     tags:
+ *     - Hello Controller
+ *     summary: Hello Post Route
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *           schema:
+ *            type: object
+ *            required:
+ *              - name
+ *            properties:
+ *              name:
+ *                type: string
+ *                default: John Doe
+ *     responses:
+ *      200:
+ *        description: Fetched Successfully
+ *      400:
+ *        description: Bad Request
+ *      404:
+ *        description: Not Found
+ *      500:
+ *        description: Server Error
+ */
+router.post('/create', validateSchema('hello', 'welcome'), welcome);
+
+/**
+ * @openapi
+ * '/hello/delete/{name}':
+ *  delete:
+ *     tags:
+ *     - Hello Controller
+ *     summary: Hello Get Route
+ *     parameters:
+ *      - name: name
+ *        in: path
+ *        description: The name (any string)
+ *        required: true
+ *     responses:
+ *      200:
+ *        description: Fetched Successfully
+ *      400:
+ *        description: Bad Request
+ *      404:
+ *        description: Not Found
+ *      500:
+ *        description: Server Error
+ */
+router.delete('/delete/:name', validateSchema('hello', 'welcome'), welcome);
+
+/**
+ * @openapi
+ * '/hello/update/{name}':
+ *  put:
+ *     tags:
+ *     - Hello Controller
+ *     summary: Hello Put Route
+ *     parameters:
+ *      - name: name
+ *        in: path
+ *        description: The name (any string)
+ *        required: true
+ *     responses:
+ *      200:
+ *        description: Fetched Successfully
+ *      400:
+ *        description: Bad Request
+ *      404:
+ *        description: Not Found
+ *      500:
+ *        description: Server Error
+ */
+router.put('/update/:name', validateSchema('hello', 'welcome'), welcome);
 
 module.exports = router;
